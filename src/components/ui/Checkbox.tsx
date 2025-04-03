1291e870c90695ee351795ffa8c1ac42377b5258
DustVoyager <ohjamdol@gmail.com>
2025-04-03 21:29:32 +0900

    checkbox component update

---
+++ src/components/ui/Checkbox.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import styles from "./Checkbox.module.css";
+
+type CheckboxSize = "sm" | "md" | "lg";
+
+interface CheckboxProps
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "size"> {
+  label?: string;
+  error?: string;
+  size?: CheckboxSize;
+}
+
+const Checkbox = React.forwardRef<HTMLInputElement, CheckboxProps>(
+  ({ label, error, size = "md", className, disabled, ...props }, ref) => {
+    const wrapperClasses = [
+      styles.checkboxWrapper,
+      disabled && styles.disabled,
+      className,
+    ]
+      .filter(Boolean)
+      .join(" ");
+
+    const checkboxClasses = [styles.checkbox, styles[size]]
+      .filter(Boolean)
+      .join(" ");
+
+    return (
+      <div>
+        <label className={wrapperClasses}>
+          <input
+            type="checkbox"
+            ref={ref}
+            className={checkboxClasses}
+            disabled={disabled}
+            {...props}
+          />
+          {label && <span className={styles.label}>{label}</span>}
+        </label>
+        {error && <span className={styles.error}>{error}</span>}
+      </div>
+    );
+  }
+);
+
+Checkbox.displayName = "Checkbox";
+
+export default Checkbox;
