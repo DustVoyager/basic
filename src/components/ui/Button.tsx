4f5bb56f966166070293bb43ced5b239214af814
DustVoyager <ohjamdol@gmail.com>
2025-04-04 18:39:48 +0900

    sass 패키지 설치에 따른 파일명 변경

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./Button.module.css";
+import styles from "./Button.module.scss";
 
 type ButtonVariant = "primary" | "secondary" | "outline" | "ghost";
 type ButtonSize = "sm" | "md" | "lg";
--- src/components/ui/Checkbox.tsx
+++ src/components/ui/Checkbox.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./Checkbox.module.css";
+import styles from "./Checkbox.module.scss";
 
 type CheckboxSize = "sm" | "md" | "lg";
 
